abb86845e0e2edacd9511613cd43225c1809d259
jun2035 <mju.6000@gmail.com>
2020-11-13 16:44:33 +0900

    商品一覧表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
   belongs_to :shipping
