fbd489ce3af74df1e9f6c7d6cd26facbac27c909
jun2035 <mju.6000@gmail.com>
2020-11-15 16:21:16 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -20,10 +22,35 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :text, :category_id, :status_id, :shipping_id, :prefecture_id, :day_id, :price, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    item = Item.find(params[:id])
+    if user_signed_in? && current_user.id != item.user_id
+      redirect_to action: :index
+    elsif !user_signed_in?
+      render template: '/devise/sessions/new'
+    end
   end
 end
