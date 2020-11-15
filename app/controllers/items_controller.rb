84554ff11370e30588852de62427a6e772507065
jun2035 <mju.6000@gmail.com>
2020-11-15 17:40:41 +0900

    【修正1】商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
 
   def index
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,13 +44,12 @@ class ItemsController < ApplicationController
           .merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    item = Item.find(params[:id])
-    if user_signed_in? && current_user.id != item.user_id
-      redirect_to action: :index
-    elsif !user_signed_in?
-      render template: '/devise/sessions/new'
-    elsif item.purchase != nil
+    if (current_user.id != @item.user_id) || (@item.purchase != nil)
       redirect_to action: :index
     end
   end
