f730f794d29620df0ae729762ed942cad097b39b
jun2035 <mju.6000@gmail.com>
2020-11-18 23:54:14 +0900

    PAY.JPを用いたクレジットカードでの決済機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,24 +9,33 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-      if@order.valid?
-        @order.save
-        redirect_to root_path
+    if @order.valid?
+      pay_item
+      @order.save
+      redirect_to root_path
     else
       render action: :index
     end
   end
 
   private
+
   # 全てのストロングパラメーターを1つに統合
   def order_params
-    params.require(:order).permit(:token, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
-  
+
   def move_to_index
     @item = Item.find(params[:item_id])
-    if (current_user.id == @item.user_id) || (@item.purchase != nil)
-      redirect_to root_path
-    end
+    redirect_to root_path if (current_user.id == @item.user_id) || !@item.purchase.nil?
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,           # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                # 通貨の種類（日本円）
+    )
   end
 end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe Order, type: :model do
+  before do
+    @order = FactoryBot.build(:order)
+  end
+
+  it 'すべての値が正しく入力されていれば保存できること' do
+    expect(@order).to be_valid
+  end
+
+  it 'tokenが空では登録できないこと' do
+    @order.token = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Token can't be blank")
+  end
+
+  it 'postal_codeが空だと保存できないこと' do
+    @order.postal_code = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Postal code can't be blank")
+  end
+  it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+    @order.postal_code = '1234567'
+    @order.valid?
+    expect(@order.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
+  end
+  it 'prefecture_idを選択していないと保存できないこと' do
+    @order.prefecture_id = 0
+    @order.valid?
+    expect(@order.errors.full_messages).to include('Prefecture Select')
+  end
+  it 'cityが空だと保存できないこと' do
+    @order.city = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("City can't be blank")
+  end
+  it 'addressが空だと保存できないこと' do
+    @order.address = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Address can't be blank")
+  end
+  it 'building_nameは空でも保存できること' do
+    @order.building_name = nil
+    expect(@order).to be_valid
+  end
+  it 'phone_numberが空だと保存できないこと' do
+    @order.phone_number = nil
+    @order.valid?
+    expect(@order.errors.full_messages).to include("Phone number can't be blank")
+  end
+  it 'phone_numberが半角数字以外だと保存できないこと' do
+    @order.phone_number = '090-1111-2222'
+    @order.valid?
+    expect(@order.errors.full_messages).to include('Phone number is invalid. Input half-width characters')
+  end
+end
